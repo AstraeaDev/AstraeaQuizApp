79eea5d08eab193dea9d39d3349ebd632b5046cc
BUGRA <camlibelbugrahan@gmail.com>
2022-12-05 14:31:01 +0300

    Show question and answers in 'Take Quiz' screen

--- app/src/main/java/com/example/astraeaquizapp/MainPage.kt
+++ app/src/main/java/com/example/astraeaquizapp/MainPage.kt
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import kotlin.reflect.typeOf
 
 class MainPage : AppCompatActivity() {
 
     var englishList = ArrayList<String>()
     var turkishList = ArrayList<String>()
-
+    var map = mapOf<String, String>()
+    var chosenWord = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +36,10 @@ class MainPage : AppCompatActivity() {
             turkishList.add(cursor.getString(turkishIndex))
         }
 
-        val map: Map<String, String> = englishList.zip(turkishList).toMap() // creates a dict
+        // close the cursor
+        cursor.close()
+
+        map = englishList.zip(turkishList).toMap() // creates a dict
 
 
         val meyveler = listOf("Elma", "Armut", "Muz", "Kivi", "Çilek", "Karpuz", "Kavun", "Ananas", "Kiraz", "Dut")
@@ -47,6 +52,7 @@ class MainPage : AppCompatActivity() {
         myList.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
 
             val secilenMeyve = parent.getItemAtPosition(position) as String
+            chosenWord = secilenMeyve
 
             CustomDialogClass(this, secilenMeyve, map).show()
 
@@ -57,6 +63,52 @@ class MainPage : AppCompatActivity() {
         val buttonClickForQuiz = findViewById<Button>(R.id.takeQuizButton)
         buttonClickForQuiz.setOnClickListener {
             val intent = Intent(this, QuizActivity::class.java )
+            intent.putExtra("dict", "$map")
+            val shuffledEnglishList = englishList.shuffled() as ArrayList<String>
+
+            // if (shuffledEnglishList.isEmpty()) {
+            //     shuffledEnglishList = listOf("No words at main page")
+            // }
+            //intent.putStringArrayListExtra("shuffledEnglishList", shuffledEnglishList)
+
+            // get turkish of the first element of shuffledEnglishList from database
+            val cursor = db.rawQuery("SELECT * FROM book WHERE english = '${shuffledEnglishList[0]}'", null)
+            val turkishIndex = cursor.getColumnIndex("turkish")
+            var turkishOfFirstElement = ""
+            while (cursor.moveToNext()) {
+                turkishOfFirstElement = cursor.getString(turkishIndex)
+            }
+            Log.d("turkishOfFirstElement", turkishOfFirstElement)
+
+            // get turkish of the second element of shuffledEnglishList from database
+            val cursor2 = db.rawQuery("SELECT * FROM book WHERE english = '${shuffledEnglishList[1]}'", null)
+            val turkishIndex2 = cursor2.getColumnIndex("turkish")
+            var turkishOfSecondElement = ""
+            while (cursor2.moveToNext()) {
+                turkishOfSecondElement = cursor2.getString(turkishIndex2)
+            }
+
+            // get turkish of the third element of shuffledEnglishList from database
+            val cursor3 = db.rawQuery("SELECT * FROM book WHERE english = '${shuffledEnglishList[2]}'", null)
+            val turkishIndex3 = cursor3.getColumnIndex("turkish")
+            var turkishOfThirdElement = ""
+            while (cursor3.moveToNext()) {
+                turkishOfThirdElement = cursor3.getString(turkishIndex3)
+            }
+
+            // get turkish of the fourth element of shuffledEnglishList from database
+            val cursor4 = db.rawQuery("SELECT * FROM book WHERE english = '${shuffledEnglishList[3]}'", null)
+            val turkishIndex4 = cursor4.getColumnIndex("turkish")
+            var turkishOfFourthElement = ""
+            while (cursor4.moveToNext()) {
+                turkishOfFourthElement = cursor4.getString(turkishIndex4)
+            }
+
+            intent.putExtra("question", shuffledEnglishList[0])
+            intent.putExtra("answer1", turkishOfFourthElement)
+            intent.putExtra("answer2", turkishOfSecondElement)
+            intent.putExtra("answer3", turkishOfThirdElement)
+            intent.putExtra("correctanswer", turkishOfFirstElement)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/astraeaquizapp/QuizActivity.kt
+++ app/src/main/java/com/example/astraeaquizapp/QuizActivity.kt
@@ -1,11 +1,42 @@
 package com.example.astraeaquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.TextView
 
 class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        // create an intent
+        val intent = Intent(this, QuizActivity::class.java)
+        // val map: Map<String, String> = intent.getSerializableExtra("map") as Map<String, String>
+        val deneme = getIntent().getStringExtra("question")
+        val answerString1 = getIntent().getStringExtra("answer1")
+        val answerString2 = getIntent().getStringExtra("answer2")
+        val answerString3 = getIntent().getStringExtra("answer3")
+        val correctAnswer = getIntent().getStringExtra("correctanswer")
+
+        var answers = listOf(answerString1, answerString2, answerString3, correctAnswer)
+        answers = answers.shuffled()
+
+
+
+        val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
+        val answer1 = findViewById<RadioButton>(R.id.radioButton)
+        val answer2 = findViewById<RadioButton>(R.id.radioButton2)
+        val answer3 = findViewById<RadioButton>(R.id.radioButton3)
+        val answer4 = findViewById<RadioButton>(R.id.radioButton4)
+
+        tvQuestion.setText(deneme)
+        answer1.setText(answers.get(0))
+        answer2.setText(answers.get(1))
+        answer3.setText(answers.get(2))
+        answer4.setText(answers.get(3))
+
     }
 }
