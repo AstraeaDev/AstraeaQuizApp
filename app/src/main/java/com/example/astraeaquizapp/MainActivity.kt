5f3467b7c27e11bf3c330903bb32a19f327fa0bb
beyzahatip <beyzahatip17@gmail.com>
2022-12-14 15:08:27 +0300

    Firebase signIn ve signUp işlemleri yapıldı

--- app/src/main/java/com/example/astraeaquizapp/MainActivity.kt
+++ app/src/main/java/com/example/astraeaquizapp/MainActivity.kt
@@ -4,19 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var fireBaseAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fireBaseAuth = FirebaseAuth.getInstance()
+
         val buttonClick = findViewById<Button>(R.id.login_button)
+
         buttonClick.setOnClickListener {
-            val intent = Intent(this, MainPage::class.java)
-            startActivity(intent)
+
+            val email = findViewById<EditText>(R.id.login_email_address).text.toString()
+            //val email = binding.registerEmail.text.toString()
+            val password = findViewById<EditText>(R.id.login_password).text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                fireBaseAuth.signInWithEmailAndPassword(email , password).addOnCompleteListener(){
+                    if(it.isSuccessful){
+                        val intent = Intent(this,MainPage::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
+
+                    }
+
+                }
+            }
+            else {
+                Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_LONG).show()
+            }
+            /* val intent = Intent(this, MainPage::class.java)
+             startActivity(intent)
+
+             */
         }
+
 
         val buttonClick2 = findViewById<Button>(R.id.registerbutton)
         buttonClick2.setOnClickListener {
--- app/src/main/java/com/example/astraeaquizapp/RegisterPage.kt
+++ app/src/main/java/com/example/astraeaquizapp/RegisterPage.kt
@@ -4,16 +4,58 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.astraeaquizapp.databinding.ActivityRegisterPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterPage : AppCompatActivity() {
+
+
+
+    private lateinit var fireBaseAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
 
+        fireBaseAuth = FirebaseAuth.getInstance()
+
+
+        val buttonClick = findViewById<Button>(R.id.register_button)
+        buttonClick.setOnClickListener{
+
+            val email = findViewById<EditText>(R.id.register_email).text.toString()
+            //val email = binding.registerEmail.text.toString()
+            val password = findViewById<EditText>(R.id.register_password).text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                fireBaseAuth.createUserWithEmailAndPassword(email , password).addOnCompleteListener(){
+                    if(it.isSuccessful){
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this, it.exception.toString(),Toast.LENGTH_LONG).show()
+
+                    }
+
+                }
+            }
+            else {
+                Toast.makeText(this, "Empty Fields Are not Allowed",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
+        /*
         val buttonClick = findViewById<Button>(R.id.register_button)
         buttonClick.setOnClickListener {
             val intent = Intent(this, MainPage::class.java)
             startActivity(intent)
         }
+        ,
+         */
     }
 }
